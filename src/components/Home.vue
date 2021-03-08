344027f28c99f2ee26553499acc4c3ee167c7291
Colton Wood <me.coltonwood@gmail.com>
2021-03-08 17:42:47 -0600

    simplifying for now

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="welcome">
+    <h1>Welcome to coltonwood.com!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.h1 {
+  margin: 0;
+}
+.welcome {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
